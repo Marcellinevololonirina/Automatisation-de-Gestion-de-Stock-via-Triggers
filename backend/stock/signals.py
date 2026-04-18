a6d745f24bdc9e552077048b8d06727aaf8053cf
Marcellinevololonirina <marcellinevololonirina@gmail.com>
2026-04-18 21:40:41 +0300

    fix:Modifier le calcul lors de l'approvisionnement

--- backend/stock/signals.py
+++ backend/stock/signals.py
@@ -3,31 +3,38 @@ from django.dispatch import receiver
 from .models import Approvisionnement, Produit, AuditApprovisionnement
 
 # STOCK + AUDIT INSERT / UPDATE
+
 @receiver(pre_save, sender=Approvisionnement)
-def update_stock_and_audit(sender, instance, **kwargs):
-    try:
+def get_old_qte(sender,instance, **kwargs):
+    if instance.pk:
         old = Approvisionnement.objects.get(pk=instance.pk)
-        ancien_qte = old.qteentree
-        action = 'UPDATE'
-    except Approvisionnement.DoesNotExist:
-        ancien_qte = 0
-        action = 'INSERT'
+        instance._old_qte=old.qteentree
+    else:
+        instance._old_qte=0
 
+
+@receiver(post_save,sender=Approvisionnement)
+def update_stock_and_audit(sender, instance,created, **kwargs):
     produit = instance.produit
+    ancien_qte = getattr(instance, '_old_qte', 0)
 
-    # Mise à jour stock
-    produit.stock = produit.stock + instance.qteentree - ancien_qte
+
+    #Mise à jour stock
+    produit.stock += instance.qteentree - ancien_qte
     produit.save()
+
+    action = 'INSERT' if created else 'UPDATE'
+
 
     # Audit
     AuditApprovisionnement.objects.create(
-        action=action,
-        fournisseur_nom=instance.fournisseur.nom,
-        produit_design=produit.design,
-        qteentree_ancien=ancien_qte,
-        qteentree_nouv=instance.qteentree,
-        utilisateur="system"  # on va améliorer après
-    )
+       action=action,
+       fournisseur_nom=instance.fournisseur.nom,
+       produit_design=produit.design,
+       qteentree_ancien=ancien_qte,
+       qteentree_nouv=instance.qteentree,
+       utilisateur="system"  # on va améliorer après
+   )
 
 
 # DELETE
